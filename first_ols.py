c7e741397998586681d0b5b9569b95374a243638
Shiraz <shiraz.baig@outlook.com>
2026-09-25 17:02:52 +0100

    Finished stockOLSEngine

--- first_ols.py
+++ first_ols.py
@@ -10,9 +10,29 @@ class stockOLSEngine:
         self.beta = None
         self.residuals = None #This means spread
 
-        self.Xdata = np.asarray(X).reshape(-1,1) #This converts list into numpy array. Then it's reshaped, -1 is to figure out how many rows, 1 is for one column. 
-        self.Ydata = np.ararray(Y).reshape(-1,1) 
+        self.Xdata = np.array(X).reshape(-1,1) #This converts the data into a numpy list. Parameters are rows and columns. -1 means figure it out from the data. 
+        self.Ydata = np.array(Y).reshape(-1,1) 
 
 
     def lineOfBestFit(self):
-        pass
+        ones = np.ones((len(self.Xdata),1)) #Column of ones with length N
+        xMatrix = np.hstack([ones,self.Xdata]) #Makes Nx2 design matrix for X
+
+        #Calculation for (X^TX)^-1
+        xTranspose = xMatrix.T
+        varianceXMatrix = xTranspose @ xMatrix
+        VXMinverse = np.linalg.inv(varianceXMatrix)
+
+        #Beta = (X^TX)^-1(X^TY)
+        covarianceMatrix = xTranspose @ self.Ydata
+        betaMatrix = VXMinverse @ covarianceMatrix
+
+        self.alpha = float(betaMatrix[0,0])
+        self.beta = float(betaMatrix[1,0])
+
+        self.residuals = self.Ydata - (xMatrix @ betaMatrix)
+
+    def returnResiduals(self):
+        return self.residuals
+
+
